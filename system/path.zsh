34174d2b825e46005da7680b2b5e624785afbd73
dc0453 <dc0453@126.com>
2021-10-22 00:09:41 +0800

    feat(zshrc): load environment variable PATH from path.env, rather than path.zsh

--- system/path.zsh
+++
@@ -1,3 +0,0 @@
-export GOPATH="$HOME/.golang"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${GOPATH}/bin/:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
