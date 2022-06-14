1dc0b44acaa0ff2e771e55f672034647e5d62aa8
dc0453 <dc0453@126.com>
2022-06-15 00:22:21 +0800

    impr(jenv): enable export plugin to set /Users/dc/.jenv/versions/1.8.0.292 properly

--- jenv/install.sh
+++ jenv/install.sh
@@ -16,3 +16,8 @@ fi
 if [[ "$IS_MAC_OS" == true ]]; then
     jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
 fi
+
+# https://github.com/jenv/jenv
+# To make sure JAVA_HOME is set, make sure to enable the export plugin:
+jenv enable-plugin export
+# exec $SHELL -l
